dddd9c320c1d9c413e0c37126ead6498f419120d
AadiXC0DE <aadityaz2077@gmail.com>
2025-03-27 01:22:27 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import "./App.css";
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Experience from "./pages/Experience";
-import Contact from "./pages/Contact";
-import Error from "./pages/Error";
+
+const Home = lazy(() => import("./pages/Home"));
+const About = lazy(() => import("./pages/About"));
+const Experience = lazy(() => import("./pages/Experience"));
+const Contact = lazy(() => import("./pages/Contact"));
+const Error = lazy(() => import("./pages/Error"));
+
+const LoadingFallback = () => (
+  <div className="tw-flex tw-items-center tw-justify-center tw-h-screen tw-bg-black">
+    <div className="tw-text-[#4fbdba] tw-text-xl">Loading...</div>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} exact />
-        <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <Suspense fallback={<LoadingFallback />}>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/about" element={<About />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
